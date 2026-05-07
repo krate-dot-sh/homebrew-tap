2fcce2958a5233d0151449bef2736aa25d31d3ef
bob durbin <bob@kannister.app>
2026-05-07 16:29:32 -0400

    feat: bump krate to v0.36.4

--- Formula/krate.rb
+++ Formula/krate.rb
@@ -1,11 +1,11 @@
 class Krate < Formula
   desc "CLI for the krate micro-VM platform"
   homepage "https://kannister.app"
-  version "0.36.3"
+  version "0.36.4"
   license "Commercial"
 
-  url "https://github.com/kannister-app/krate/releases/download/v0.36.3/krate-0.36.3-arm64-darwin.tar.gz"
-  sha256 "11445ba9fb73785080a6a4c9a307499fe4b5a4a73b922ac51ae49fc7f8413998"
+  url "https://github.com/kannister-app/krate/releases/download/v0.36.4/krate-0.36.4-arm64-darwin.tar.gz"
+  sha256 "d72095dd5ac6f91848df1f1c2170b7582f4afee0b0bed5d678743f0258601613"
 
   depends_on :macos
   depends_on arch: :arm64
